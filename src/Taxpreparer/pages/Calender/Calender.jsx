3d5bd08c68dfd56568c85341f897592eaaa6913f
neeru <neeru@itinfonity.com>
2026-03-11 11:52:36 +0530

    added code

--- src/Taxpreparer/pages/Calender/Calender.jsx
+++ src/Taxpreparer/pages/Calender/Calender.jsx
@@ -921,6 +921,7 @@ export default function CalendarPage() {
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
               </button>
             </div>
+
             {/* Body */}
             <div className="p-6 space-y-4">
               {/* Event Title */}
